bc9c283add31538964425bfdc16eaabe43a57d25
Jiang <hblolj@gmail.com>
2019-04-11 23:52:10 +0800

    Init

---
+++ src/main/java/com/hblolj/springtransactionlearn/SpringTransactionLearnApplication.java
@@ -0,0 +1,13 @@
+package com.hblolj.springtransactionlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionLearnApplication.class, args);
+    }
+
+}
